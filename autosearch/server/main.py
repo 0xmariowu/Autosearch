1d07730094767e896d14180f43f57997fc746c49
0xmariowu <130952152+0xmariowu@users.noreply.github.com>
2026-04-17 20:53:29 +0800

    fix: scope CORS to dev origins + NO_COLOR for typer help test

--- autosearch/server/main.py
+++ autosearch/server/main.py
@@ -26,12 +26,20 @@ class SearchRequest(BaseModel):
 
 
 app = FastAPI(title="AutoSearch", version=__version__)
+_DEV_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8080",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=_DEV_ALLOWED_ORIGINS,
     allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type"],
 )
 
 
